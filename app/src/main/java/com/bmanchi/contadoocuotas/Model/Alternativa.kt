3314b7e4f889f2100c929349f329efd91ff76580
Bruno <b.manchinelli@gmail.com>
2020-12-30 13:47:26 -0300

    adapter para tabla

---
+++ app/src/main/java/com/bmanchi/contadoocuotas/Model/Alternativa.kt
@@ -0,0 +1,12 @@
+package com.bmanchi.contadoocuotas.Model
+
+// TODO: Data Class?
+
+class Alternativa (var precio: Double, var descuento: Double, var cantidadCuotas:Int, var precioFinal: Double) {
+    /*precio = null
+    var descuento = null
+    var cantidadCuotas = null
+    var interes = null
+    var final = null*/
+
+}
