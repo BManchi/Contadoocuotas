89e7b200d450bb34ca529c399be9647699585060
Bruno <b.manchinelli@gmail.com>
2021-01-03 13:30:51 -0300

    alternativa Recyclerview?

--- app/src/main/java/com/bmanchi/contadoocuotas/View/MainActivity.kt
+++ app/src/main/java/com/bmanchi/contadoocuotas/View/MainActivity.kt
@@ -2,13 +2,22 @@ package com.bmanchi.contadoocuotas.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bmanchi.contadoocuotas.AlternativaAdapter
+import com.bmanchi.contadoocuotas.R
 import com.bmanchi.contadoocuotas.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
+    private var myDataset = arrayOf("texto 1", "texto 2" )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,19 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = AlternativaAdapter(myDataset)
 
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
     }
 }
