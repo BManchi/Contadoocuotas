c61a9f6c9da061238f58628bd9c3ef243ff1647e
Bruno <b.manchinelli@gmail.com>
2021-03-01 16:18:35 -0300

    limpiar código

--- app/src/main/java/com/bmanchi/contadoocuotas/view/MainActivity.kt
+++ app/src/main/java/com/bmanchi/contadoocuotas/view/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.bmanchi.contadoocuotas.View
+package com.bmanchi.contadoocuotas.view
 
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
-import com.bmanchi.contadoocuotas.ViewModel.CalcularViewModel
+import com.bmanchi.contadoocuotas.viewModel.CalcularViewModel
 import com.bmanchi.contadoocuotas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,24 +19,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         val model: CalcularViewModel by viewModels()
-        // TODO traer data del VM
 
-        /** Si el text del editText es Empty, pone 0 en la variable a modificar */
-        /** Igual creo que se puede modificar y achicar */
         binding.precioContado.doAfterTextChanged {
-
-            if (it.toString().isEmpty()) {
-                model.precioContado = 0.00  //TODO estas var están insanciadas en 0 en el VM. Se podría eliminar este if
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.precioContado = it.toString().toDouble()
             }
             model.calcularContado()
         }
 
         binding.descuentoContado.doAfterTextChanged {
-            if (it.toString().isEmpty()) {
-                model.descuento = 0
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.descuento = it.toString().toInt()
             }
             model.calcularContado()
@@ -47,39 +39,39 @@ class MainActivity : AppCompatActivity() {
         })
 
         binding.precioCuotas.doAfterTextChanged {
-            if (it.toString().isEmpty()) {
-                model.precioCuotas = 0.00
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.precioCuotas = it.toString().toDouble()
             }
             model.calcularCuotas()
         }
 
         binding.cuotas.doAfterTextChanged {
-            if (it.toString().isEmpty()) {
-                model.cantidadCuotas = 0
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.cantidadCuotas = it.toString().toInt()
             }
             model.calcularCuotas()
         }
 
         binding.interes.doAfterTextChanged {
-            if (it.toString().isEmpty()) {
-                model.interes = 0
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.interes = it.toString().toInt()
             }
             model.calcularCuotas()
         }
+
+        binding.finalCuotas.doAfterTextChanged {
+            if (it.toString().isNotEmpty()) {
+                model.finalCuotas = it.toString().toDouble()
+            }
+            model.calcularCuotas()
+        }
+
         model.finalDescontadoLiveData.observe(this, { text ->
             binding.finalDescontado.text = text
         })
 
         binding.inflacionMensual.doAfterTextChanged {
-            if (it.toString().isEmpty()) {
-                model.inflacion = 0.00
-            } else {
+            if (it.toString().isNotEmpty()) {
                 model.inflacion = it.toString().toDouble()
             }
             model.calcularCuotas()
--- app/src/main/java/com/bmanchi/contadoocuotas/viewModel/CalcularViewModel.kt
+++ app/src/main/java/com/bmanchi/contadoocuotas/viewModel/CalcularViewModel.kt
@@ -1,4 +1,4 @@
-package com.bmanchi.contadoocuotas.ViewModel
+package com.bmanchi.contadoocuotas.viewModel
 
 import android.util.Log
 import androidx.lifecycle.LiveData
@@ -11,30 +11,28 @@ private const val TAG = "calcularViewModel"
 
 class CalcularViewModel: ViewModel() {
 
-    //todo br1: Todos estos objetos son realmente nulleables? porque si le das valor 0 al instancearlos
-    //todo br1: y nunca los pasas a null, debielas sacarle el signo de pregunta
-    var precioContado: Double? = 0.0
-    var descuento: Int? = 0
-    var finalContado = MutableLiveData<Double>()
+    var precioContado = 0.0
+    var descuento: Int = 0
+    private var finalContado = MutableLiveData<Double>()
     val finalContadoLiveData : LiveData<String>
         get() = Transformations.map(finalContado) {it.toString()}
 
-    var precioCuotas:Double? = 0.0
-    var cantidadCuotas:Int? = 0
-    var interes:Int? = 0
-    var finalCuotas:Double? = 0.0
-    var finalDescontado = MutableLiveData<Double>()
+    var precioCuotas = 0.0
+    var cantidadCuotas = 0
+    var interes = 0
+    var finalCuotas = 0.0
+    private var finalDescontado = MutableLiveData<Double>()
     val finalDescontadoLiveData : LiveData<String>
         get() = Transformations.map(finalDescontado) {it.toString()}
 
-    var inflacion:Double? = 0.0
+    var inflacion = 0.0
 
     var mejorAlternativa = MutableLiveData<String>()
 
 
     fun calcularContado(){
-        if (precioContado!! > 0 && descuento!! > 0) {
-            finalContado.value = precioContado?.times((1 - (descuento?.toDouble()?.div(100)!!)))
+        if (precioContado > 0 && descuento > 0) {
+            finalContado.value = precioContado.times((1 - (descuento.toDouble().div(100))))
 
             Log.d(TAG, "calularContado() finalContado = ${finalContado.value}")
 
@@ -51,27 +49,27 @@ class CalcularViewModel: ViewModel() {
 
     fun calcularCuotas(){
         Log.d(TAG, "calularCuotas()")
-        if (precioCuotas!! > 0 && cantidadCuotas!! > 0) {
-            if (interes!! == 0){
+        if (precioCuotas > 0 && cantidadCuotas > 0) {
+            if (interes == 0){
                 finalCuotas = precioCuotas
                 Log.d(TAG, "finalCuotas = $finalCuotas")
                 finalDescontado.value = String.format("%.2f",
-                    finalCuotas?.div((1+ inflacion!!.toDouble().div(100)).pow(cantidadCuotas!!))).toDouble()
+                    finalCuotas.div((1+ inflacion.div(100)).pow(cantidadCuotas))).toDouble()
             }
 
-            if (interes!! > 0){
+            if (interes > 0){
                 finalCuotas = String.format("%.2f",
-                    precioCuotas?.times((1 + (interes?.toDouble()?.div(100)!!)))).toDouble()
+                    precioCuotas.times((1 + (interes.toDouble().div(100))))).toDouble()
                 Log.d(TAG, "finalCuotas = $finalCuotas")
                 finalDescontado.value = String.format("%.2f",
-                    finalCuotas?.div((1+ inflacion!!.toDouble().div(100)).pow(cantidadCuotas!!))).toDouble()
+                    finalCuotas.div((1+ inflacion.div(100)).pow(cantidadCuotas))).toDouble()
                 Log.d(TAG, "finalDescontado = ${finalDescontado.value}")
             }
         }
         calcularMejor()
     }
 
-    fun calcularMejor() {
+    private fun calcularMejor() {
         Log.d(TAG, "calularMejor()")
         if (finalContado.value != null && finalDescontado.value != null){
             if (finalContado.value!! > finalDescontado.value!!) {
